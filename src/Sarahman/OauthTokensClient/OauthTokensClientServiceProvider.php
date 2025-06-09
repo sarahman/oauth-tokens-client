1fee52f90af80909c46d6b7612d27198ad7b33eb
Syed Abidur Rahman <aabid048@gmail.com>
2025-06-09 21:24:10 +0000

    Initialize the library package.

---
+++ src/Sarahman/OauthTokensClient/OauthTokensClientServiceProvider.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Sarahman\OauthTokensClient;
+
+use Illuminate\Support\ServiceProvider;
+
+class OauthTokensClientServiceProvider extends ServiceProvider
+{
+    /**
+     * Indicates if loading of the provider is deferred.
+     *
+     * @var bool
+     */
+    protected $defer = true;
+
+    /**
+     * Bootstrap the application events.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->package('sarahman/oauth-tokens-client');
+    }
+
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides()
+    {
+        return array('oauth-tokens-client.service');
+    }
+}
